1a1fe0b7c8e5cd13c655e44ed761db2a4ccd7d0a
bibek-thapa <bibek.thapahere1@gmail.com>
2018-12-06 13:43:44 -0500

    Working code

--- src/main/java/client/ChatClientInterface.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ChatClientInterface extends Remote{
-
-		public void sendMessage(String message) throws RemoteException;	
-}
